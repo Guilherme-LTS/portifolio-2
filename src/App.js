dec3c0a7f2326b66eb59aea2e609c07c89783679
Guilherme-LTS <glts.snf21@uea.edu.br>
2025-07-08 18:19:48 -0400

    Added the Profile Card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import ProfileCard from './components/ProfileCard';
 import './App.css';
 
 function App() {
@@ -8,7 +9,26 @@ function App() {
       <Navbar />
       <main>
         <section id="home">
-          <h1>Bem-vindo ao meu portfólio!</h1>
+          <div className="home-content">
+            <ProfileCard
+              name="Guilherme Silva"
+              title="Dev em Evolução e Empreendedor"
+              handle="guilherme.lts"
+              status="Online"
+              contactText="Contate me"
+              avatarUrl="https://avatars.githubusercontent.com/u/99211563?v=4"
+              showUserInfo={true}
+              enableTilt={true}
+              onContactClick={() => console.log('Contact clicked')}
+              iconUrl="/images/iconpattern.png"
+            />
+            <div className="home-text">
+              <h1>Bem-vindo ao meu portfólio!</h1>
+              <p>
+                Sou apaixonado por tecnologia, desenvolvimento web e sempre em busca de novos desafios. Aqui você vai encontrar meus principais projetos, experiências e formas de contato.
+              </p>
+            </div>
+          </div>
         </section>
         <section id="sobre">
           <h2>Sobre mim</h2>
