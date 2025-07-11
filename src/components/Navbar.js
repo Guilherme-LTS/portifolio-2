4ca9d1731e005dd31c1606143bf989d58bb9ffe2
Guilherme-LTS <glts.snf21@uea.edu.br>
2025-07-11 19:35:42 -0400

    Arrumando nav bar e adicionando novos efeitos v.0.5

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,37 @@
 import React from 'react';
+import { FaUser, FaTools, FaGift, FaCode, FaCertificate, FaTrophy, FaPhone, FaRegMoon } from 'react-icons/fa';
 import './Navbar.css';
+import GooeyNav from './GooeyNav';
 
 function Navbar() {
+  const items = [
+    { label: "Home", href: "#home" },
+    { label: "About", href: "#about" },
+    { label: "Projects", href: "#projects" },
+    { label: "Contact", href: "#contact" },
+  ];
+
   return (
     <nav className="navbar-lateral">
-      <ul>
-        <li><a href="#home">Início</a></li>
-        <li><a href="#sobre">Sobre</a></li>
-        <li><a href="#projetos">Projetos</a></li>
-        <li><a href="#contato">Contato</a></li>
-      </ul>
+      <div className="navbar-logo">
+        {/* Substitua pelo seu logo real se quiser */}
+        <span className="logo-icon">L7</span>
+      </div>
+      <GooeyNav
+        items={items}
+        particleCount={15}
+        particleDistances={[90, 10]}
+        particleR={100}
+        initialActiveIndex={0}
+        animationTime={600}
+        timeVariance={300}
+        colors={[1, 2, 3, 1, 2, 3, 1, 4]}
+      />
+      <div className="navbar-footer">
+        <button className="navbar-footer-btn">
+          <FaRegMoon />
+        </button>
+      </div>
     </nav>
   );
 }
