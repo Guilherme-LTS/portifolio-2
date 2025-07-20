eb0fd64558fe1eea233200d772b3daa51c6c8e09
Guilherme-LTS <glts.snf21@uea.edu.br>
2025-07-20 13:32:54 -0400

    fixed some bugs

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,15 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Ignora warnings de source maps de dependências
+console.warn = (msg, ...args) => {
+  if (
+    typeof msg === "string" &&
+    msg.includes("Failed to parse source map")
+  ) {
+    return;
+  }
+  // Chama o console.warn original
+  return global.console.warn(msg, ...args);
+};
